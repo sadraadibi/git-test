f61fb45f44d29303934e00f068e89517a932970d
sadraadibi <sadraadibi@aut.ac.ir>
2019-12-29 23:04:34 +0330

    Update test.c

--- test.c
+++ test.c
@@ -2,9 +2,25 @@
 
 int main() 
 {
-	int x1,x2,x3,x4,x5,x6,t;
+	int x1,x2,x3,x4,x5,x6,v1,v2,v3,t;
 	scanf("%d\n",&t);
-//houhuihiuhiuguygyggkyggyugyuk
+
+	while (t) {
+	scanf("%d",&x1);
+	if (x1==1){
+				printf("Jab\n");
+	}
+	if (x1==2){
+				printf("Straight Right\n");
+	}
+	if (x1==3){
+				printf("Hook\n");
+	}
+	if (x1==4){
+				printf("Upper Cut\n");
+	}
+	--t;
+}
 	return 0;
 	
 	
